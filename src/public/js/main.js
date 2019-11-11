4b2991992c77931550cffe6eefd614965596b299
jhonf19 <zjhon2018@gmail.com>
2019-11-11 13:29:36 -0500

    chat go ahead

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-    
+
     const socket = io();
 
     //get DOM ids
@@ -7,15 +7,53 @@ $(function () {
     const msg = $('#msg');
     const chat = $('#chat');
 
+    const logForm = $('#log-form');
+    const userName = $('#username');
+    const userError = $('#user-error');
+
+    const userNames = $('#usernames');
+
+    logForm.submit(e => {
+        e.preventDefault();
+        socket.emit('new user', userName.val(), (data) => {
+            if (data) {
+                $('#content_log').hide();
+                $('#content_chat').show();
+                $('#content_chat').css('display','flex');
+            } else {
+                userError.html(`
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <strong>Error!</strong> nombre de usuario en uso.
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                    </div>
+                `);
+            }
+        });
+        userName.val('')
+    });
+
+
+
     //events
-    formMsg.submit( e => {
+    formMsg.submit(e => {
         e.preventDefault();
         socket.emit('send msg', msg.val());
         msg.val('');
     });
 
-    socket.on('new msg', (msg)=>{
-        chat.append(`${msg}<br><hr>`);
+    socket.on('new msg', (msg) => {
+        chat.append(`<b>${msg.user}:</b> ${msg.msg}<br><hr>`);
+    })
+
+    socket.on('usernames', data => {
+        let html = '';
+        for (let i = 0; i < data.length; i++) {
+            html += ` <li>${data[i]}</li>`;
+            
+        }
+        userNames.html(html);
     })
 
 })
--- src/sockets.js
+++ src/sockets.js
@@ -1,12 +1,43 @@
-
-
 module.exports = (io)=>{
+
+    let usernames = [];
+
     io.on('connection', (socket) =>{
         console.log('connect', socket.id);
 
         socket.on('send msg', (msg)=>{
-            io.sockets.emit('new msg', msg);
+            io.sockets.emit('new msg', {
+                msg: msg,
+                user: socket.username
+            });
         })
+
+        socket.on('new user', (data, cb) => {
+            console.log(data);
+            
+            if (usernames.indexOf(data) != -1) {
+                cb(false);
+            }else{
+                cb(true)
+                socket.username = data;
+                usernames.push(socket.username);
+            }
+            updateUser()
+        })
+
+        socket.on('disconnect', data => {
+            if (!socket.username) {
+                return;
+            }
+            usernames.splice(usernames.indexOf(socket.username), 1)
+            updateUser()
+        })
+
+
+        function updateUser() {
+            io.sockets.emit('usernames', usernames)
+        }
+
         
     })
     
